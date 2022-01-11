09d8088f53da8d094c09cea39499285ebb8a6693
Sergey Dyudin <sergeydyudin@yandex.ru>
2022-01-11 12:46:02 +0300

    Registered book's models at admin. Update models.
    Squash migration file.

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 
-from .models import Items
+from .models import Items, Books, Authors, Categories, Genres, Publishers, Languages
 
 
 class ItemsAdmin(admin.ModelAdmin):
     prepopulated_fields = {
-        "slug": ("title", )
+        'slug': ('title', )
     }
+
+
+class BooksAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('title', )
+    }
+
+
+admin.site.register(Items, ItemsAdmin)
+admin.site.register(Books, BooksAdmin)
+admin.site.register(Authors)
+admin.site.register(Categories)
+admin.site.register(Genres)
+admin.site.register(Publishers)
+admin.site.register(Languages)
--- books/models.py
+++ books/models.py
@@ -16,6 +16,13 @@ class Items(models.Model):
     available = models.PositiveSmallIntegerField(_('available'), default=0, blank=False)
     slug = models.SlugField(_('URL'), unique=True, blank=False)
 
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
+
+    def __str__(self):
+        return self.title
+
     def save_with_slug(self, *args, **kwargs):
         self.slug = slugify(''.join(alphabet.get(w, w) for w in self.title.lower()))
         super(Items, self).save(*args, **kwargs)
@@ -27,6 +34,11 @@ class Languages(models.Model):
 
     class Meta:
         unique_together = ['code', 'language']
+        verbose_name = 'Language'
+        verbose_name_plural = 'Languages'
+
+    def __str__(self):
+        return self.language
 
 
 class Publishers(models.Model):
@@ -35,21 +47,47 @@ class Publishers(models.Model):
 
     class Meta:
         unique_together = ['name', 'address']
+        verbose_name = 'Publisher'
+        verbose_name_plural = 'Publishers'
+
+    def __str__(self):
+        return self.name
 
 
 class Authors(models.Model):
     name = models.CharField(_('author'), max_length=50, blank=False)
-    description = models.TextField(_('description'))
+    description = models.TextField(_('description'), blank=True)
     photo = models.ImageField(verbose_name=_("author's photo"), upload_to='books/authors_photo', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+    def __str__(self):
+        return self.name
 
 
 class Genres(models.Model):
     name = models.CharField(_('genre'), max_length=30, unique=True, blank=False)
 
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
+    def __str__(self):
+        return self.name
+
 
 class Categories(models.Model):
     name = models.CharField(_('category'), max_length=50, blank=True, unique=True)
-    description = models.TextField(_('description'))
+    description = models.TextField(_('description'), blank=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Books(Items):
