8c0812eaa3223b8109b0a0d5a67f7e911a0d8fb8
Sergey Dyudin <sergeydyudin@yandex.ru>
2022-02-02 11:11:38 +0300

    Add photo to item (media/)

--- books/urls.py
+++ books/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -8,4 +10,5 @@ urlpatterns = [
     path('category/<str:cat>', views.CategoryListView.as_view(), name='category'),
     path('type/<str:type>', views.TypeListView.as_view(), name='type'),
     path('<slug:slug>/', views.ItemDetailView.as_view(), name='item')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
